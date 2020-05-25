2690d2c59504af0e1df20defcca3057d4937e023
Silas-fp <silasf_primiani@hotmail.com>
2020-05-25 18:24:30 -0300

    26. Form de Contato do Curso.

---
+++ simplemooc/courses/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class ContactCourse(forms.Form):
+    name = forms.CharField(label='Nome', max_length=100)
+    email = forms.EmailField(label='E-mail')
+    message = forms.CharField(
+        label='Mensagem/Duvida', widget=forms.Textarea
+    )
