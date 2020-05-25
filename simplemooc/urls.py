12e08c3dad2dcc85346c47b5e80584afd857d698
Silas-fp <silasf_primiani@hotmail.com>
2020-05-24 23:27:31 -0300

    21. Trabalhando com as imagens dos cursos.

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -24,9 +24,14 @@ urlpatterns = [
 from django.contrib import admin 
 from django.urls import include, path 
 from simplemooc.core import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [ 
 	path('', include('simplemooc.core.urls', namespace='core')),
     path('cursos/', include('simplemooc.courses.urls', namespace='courses')),
 	path('admin/', admin.site.urls), 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
