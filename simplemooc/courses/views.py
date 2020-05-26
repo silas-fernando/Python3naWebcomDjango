1baaae3358f7cc8a029fe7642dcb666740a5a25e
Silas-fp <silasf_primiani@hotmail.com>
2020-05-25 22:09:43 -0300

    28. Submetendo o Form do Curso.

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -22,13 +22,16 @@ def details(request, pk):
 """
 def details(request, slug):
 	course = get_object_or_404(Course, slug=slug)
+	context = {}
 	if request.method == 'POST':
 		form = ContactCourse(request.POST)
+		if form.is_valid():
+			context['is_valid'] = True
+			print(form.cleaned_data)
+			form = ContactCourse()
 	else:
 		form = ContactCourse()
-	context = {
-		'course': course,
-		'form': form
-	}
+	context['form'] = form
+	context['course'] = course
 	template_name = 'courses/details.html'
 	return render(request, template_name, context)
