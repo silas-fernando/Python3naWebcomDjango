4186d926c7e289fcbece88b5d96c4762d63f62ec
Silas-fp <silasf_primiani@hotmail.com>
2020-05-30 21:11:52 -0300

    33. View de Login.

---
+++ simplemooc/accounts/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path 
+#OBSOLETO: from django.contrib import auth 
+from django.contrib.auth.views import LoginView
+from django.urls import path
+
+app_name = 'accounts'
+"""
+OBSOLETO:
+urlpatterns = [ 
+	path('entrar', auth.views.login, 
+    {'template_name': 'accounts/login.html'}, name='login'),
+]
+"""
+urlpatterns = [ 
+	path('entrar', LoginView.as_view(), 
+    {'template_name': 'accounts/login.html'}, name='login'),
+]
