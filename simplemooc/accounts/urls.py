32544f9b1bb1c1298d0305a2f576b08ef22ee1a4
Silas-fp <silasf_primiani@hotmail.com>
2020-06-01 21:03:59 -0300

    40. Template do Painel do Usuário.

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
 ]
 """
 urlpatterns = [ 
+    path('', views.dashboard, name='dashboard'),
 	path('entrar/', LoginView.as_view(template_name='accounts/login.html'), 
          name='login'),
     path('sair/', LogoutView.as_view(next_page='core:home'), 
--- simplemooc/accounts/views.py
+++ simplemooc/accounts/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.conf import settings
 
 from .forms import RegisterForm
+
+@login_required # Verifica se o usuário está logado. Se sim, mostra o painel do usuário. Senão, redireciona para a página de login.
+def dashboard(request):
+    template_name = 'accounts/dashboard.html'
+    return render(request, template_name)
+
 
 def register(request):
     template_name = 'accounts/register.html'
