c450e7003ef684769799393b3b84ba177227600d
Silas-fp <silasf_primiani@hotmail.com>
2020-06-26 22:17:50 -0300

    65. Admin das Aulas.

--- simplemooc/courses/admin.py
+++ simplemooc/courses/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Course, Enrollment, Announcement, Comment
+from .models import Course, Enrollment, Announcement, Comment, Lesson, Material
 
 class CourseAdmin(admin.ModelAdmin):
 
@@ -8,5 +8,21 @@ class CourseAdmin(admin.ModelAdmin):
 	search_fields = ['name', 'slug']
 	prepopulated_fields = {'slug': ('name',)}
 
+# Material sempre será cadastrado inline, ou seja, junto com o model principal que nesse caso é Lesson.
+class MaterialInlineAdmin(admin.StackedInline): # Stacked indica a disposição dos campos do model inline.
+
+	model = Material
+
+class LessonAdmin(admin.ModelAdmin):
+
+	list_display = ['name', 'number', 'course', 'release_date']
+	search_fields = ['name', 'description']
+	list_filter = ['created_at']
+
+	inlines = [
+		MaterialInlineAdmin
+	]
+
 admin.site.register(Course, CourseAdmin)
-admin.site.register([Enrollment, Announcement, Comment])
+admin.site.register([Enrollment, Announcement, Comment, Material])
+admin.site.register(Lesson, LessonAdmin)
