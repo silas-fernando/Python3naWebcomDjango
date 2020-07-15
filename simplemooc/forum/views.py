a708b5e839a6f29bdd9a07d52466cfe120775893
Silas-fp <silasf_primiani@hotmail.com>
2020-07-15 02:26:22 -0300

    88. Resposta correta via ajax 2.

--- simplemooc/forum/views.py
+++ simplemooc/forum/views.py
@@ -1,6 +1,8 @@
+import json
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import (TemplateView, View, ListView, DetailView)
 from django.contrib import messages
+from django.http import HttpResponse
 
 from .models import Thread, Reply
 from .forms import ReplyForm
@@ -93,11 +95,16 @@ class ReplyCorrectView(View):
     correct = True
 
     def get(self, request, pk):
-        reply = get_object_or_404(Reply, pk=pk, author=request.user)
+        reply = get_object_or_404(Reply, pk=pk, thread__author=request.user)
         reply.correct = self.correct
         reply.save()
-        messages.success(request, 'Resposta atualizada com sucesso.')
-        return redirect(reply.thread.get_absolute_url())
+        message = 'Resposta atualizada com sucesso.'
+        if request.is_ajax():
+            data = {'Success': True, 'message': message}
+            return HttpResponse(json.dumps(data), content_type='application/json')
+        else:
+            messages.success(request, message)
+            return redirect(reply.thread.get_absolute_url())
 
 
 # as_view() Transforma a classe ForumView em uma função que pode ser usada como uma view.
